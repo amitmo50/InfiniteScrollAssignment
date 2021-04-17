37060ffe0987892857b00717f91a8b6bebb2221f
Amit Mor <amitmo50@gmail.com>
2021-04-17 20:12:43 +0300

    Amit: Update heroku file path

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-infinite-scroll-app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-infinite-scroll-app/'}),
-);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/angular-infinite-scroll-app/index.html'))
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
